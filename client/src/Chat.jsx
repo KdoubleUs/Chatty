77c0a45f26758f89e1941f1647f382375c909041
KW <88893976+KdoubleUs@users.noreply.github.com>
2022-06-06 11:21:58 -0400

    bug

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 function Chat({ socket, username, room }) {
+  const [connected, setConnected] = useState(false);
   const [message, setMessage] = useState("");
   const [messageList, setMessagelist] = useState([]);
   const sendMessage = async () => {
@@ -15,12 +16,13 @@ function Chat({ socket, username, room }) {
           new Date(Date.now()).getMinutes(),
       };
       await socket.emit("send_message", messageData);
-      setMessagelist(msg => [...msg, messageData]);
+      setMessagelist(list => [...list, messageData]);
     }
   };
   const messageOnChange = e => {
     setMessage(e.target.value);
   };
+
   useEffect(() => {
     socket.on("recieved_message", data => {
       setMessagelist(list => [...list, data]);
@@ -33,7 +35,17 @@ function Chat({ socket, username, room }) {
       </div>
       <div className="chat-body">
         {messageList.map(msg => {
-          return <h1>{msg.message}</h1>;
+          return (
+            <div>
+              <div className="message-content">
+                <p>{msg.message}</p>
+              </div>
+              <div className="message-meta">
+                <p id="time">{msg.time}</p>
+                <p id="author">{msg.author}</p>
+              </div>
+            </div>
+          );
         })}
       </div>
       <div className="chat-footer">
